a2d4b4ef37da0de309ab6ff66e62731f2e992f0f
Emmanuel <fegomson@gmail.com>
2020-10-26 19:39:36 -0500

    implement styled components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // React Icons
 import {FaHome} from "react-icons/fa"
 
+// Styled Components
+import styled from "styled-components"
+
 function App() {
   return (
     <div className="App">
       <h1><FaHome /> Techzone</h1>
-
+      <Button>Click me</Button>
     </div>
   );
 }
+
+const Button = styled.button`
+  background: var(--primary-color);
+  color: var(--mainWhite);
+  font-size: ${(props) => props.large ? "3rem" : "inherit"};
+`
 
 export default App;
