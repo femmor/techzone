98c8849549a4211e728bb6d148cc1b93cd3e0c3d
Emmanuel <fegomson@gmail.com>
2020-10-26 19:32:22 -0500

    implement react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+// React Icons
+import {FaHome} from "react-icons/fa"
+
 function App() {
   return (
     <div className="App">
-      <h1>Techazon</h1>
+      <h1><FaHome /> Techzone</h1>
+
     </div>
   );
 }
